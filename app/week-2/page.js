bbda2fe0dbd4fa7af8518c8b5de9a11fcb510bf7
Sarah <snp.orbenet@gmail.com>
2024-01-15 15:16:12 -0700

    Changing div tag to main

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -3,13 +3,13 @@ import StudentInfo from "./student-info";
 export default function Page()
 {
     return(
-        <div className="min-h-screen bg-black">
+        <main className="min-h-screen bg-black">
             <h1 className="text-gray-100">
                 Shopping List
             </h1>
             <div className="text-gray-100">
                 <StudentInfo/>
             </div>
-        </div>
+        </main>
     );
 }
