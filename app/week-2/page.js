10cc9c3cf9355a46fd3ca4c419e611b83030a937
Sarah <snp.orbenet@gmail.com>
2024-01-15 15:01:54 -0700

    Changed root file

---
+++ app/week-2/page.js
@@ -0,0 +1,15 @@
+import StudentInfo from "./student-info";
+
+export default function Page()
+{
+    return(
+        <div className="min-h-screen bg-black">
+            <h1 className="text-gray-100">
+                Shopping List
+            </h1>
+            <div className="text-gray-100">
+                <StudentInfo/>
+            </div>
+        </div>
+    );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function StudentInfo()
+{
+    return(
+        <div>
+            <div>
+                Sarah Tenebro
+            </div>
+            <div>
+                <Link href="https://github.com/SarahNT1">
+                    GitHub
+                </Link>
+            </div>
+        </div>
+    );
+}
